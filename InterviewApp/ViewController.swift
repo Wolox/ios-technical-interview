c4e185e0ccd366df726430c5adc2647259e36e1b
Diego Quiros <diegoaquiros@gmail.com>
2019-04-10 10:02:25 -0300

    initial commit

---
+++ InterviewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterviewApp
+//
+//  Created by Diego Quiros on 10/04/2019.
+//  Copyright © 2019 Diego Quiros. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
